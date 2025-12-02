8d3181bd8f17499d59308b4160a3e4b04b1847fa
Teo Sable <sobolevskii.fa@phystech.edu>
2025-12-02 13:11:05 +0300

    Import fixes and demo updates
    Fixed imports in methods (laplace_approximation.py and variational_renyi.py) and in notebooks, as well as tidied up VR and PVI notebooks

--- bensemble/methods/laplace_approximation.py
+++ bensemble/methods/laplace_approximation.py
@@ -7,7 +7,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch.utils.data import DataLoader
 
-from core.base import BaseBayesianEnsemble
+from ..core.base import BaseBayesianEnsemble
 
 
 class LaplaceApproximation(BaseBayesianEnsemble):
--- bensemble/methods/variational_renyi.py
+++ bensemble/methods/variational_renyi.py
@@ -5,7 +5,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-from core.base import BaseBayesianEnsemble
+from ..core.base import BaseBayesianEnsemble
 
 
 class VariationalRenyi(BaseBayesianEnsemble):
