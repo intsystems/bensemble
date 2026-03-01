a4337241447fb038f8e923fc66866eff7189f097
Dmitry Vasilenko <deenixie@gmail.com>
2026-03-01 17:21:38 +0300

    feat(uncertainty): implement decompose_regression_uncertainty()

--- bensemble/uncertainty.py
+++ bensemble/uncertainty.py
@@ -6,7 +6,7 @@ def decompose_classification_uncertainty(
     probs: torch.Tensor, eps: float = 1e-8
 ) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
     """
-    Decomposes total predictive uncertainty into aleatoric and epistemic components.
+    Decomposes total predictive uncertainty for classification into aleatoric and epistemic components.
 
     Args:
         probs (torch.Tensor): Predicted probabilities from the ensemble.
@@ -25,5 +25,30 @@ def decompose_classification_uncertainty(
     entropies = -torch.sum(probs * torch.log(probs + eps), dim=-1)
     aleatoric_unc = entropies.mean(dim=0)
     epistemic_unc = total_unc - aleatoric_unc
+
+    return total_unc, aleatoric_unc, epistemic_unc
+
+
+def decompose_regression_uncertainty(
+    means: torch.Tensor, variances: torch.Tensor
+) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+    """
+    Decomposes total predictive uncertainty for regression into aleatoric and epistemic components.
+
+    Args:
+        means (torch.Tensor): Predicted means from the ensemble.
+            Expected shape: [M_models, Batch_size, Out_dim].
+        variances (torch.Tensor): Predicted variances from the ensemble.
+            Expected shape:[M_models, Batch_size, Out_dim].
+
+    Returns:
+        Tuple[torch.Tensor, torch.Tensor, torch.Tensor]: A tuple containing:
+            - total_unc (torch.Tensor): Total uncertainty (entropy of the mean). Shape: [Batch].
+            - aleatoric_unc (torch.Tensor): Aleatoric uncertainty (mean of entropies). Shape: [Batch].
+            - epistemic_unc (torch.Tensor): Epistemic uncertainty (mutual information). Shape: [Batch].
+    """
+    aleatoric_unc = variances.mean(dim=0)
+    epistemic_unc = means.var(dim=0, unbiased=False)
+    total_unc = aleatoric_unc + epistemic_unc
 
     return total_unc, aleatoric_unc, epistemic_unc
