77618de62c8bf582ed21081be0f80fc1f7b172f7
Dmitry Vasilenko <deenixie@gmail.com>
2026-03-29 13:42:01 +0300

    feat(nes-bayesian): implement empty class for NESBayesianSampler

---
+++ bensemble/search/bayesian.py
@@ -0,0 +1,35 @@
+from torch.utils.data import DataLoader
+
+from bensemble.core.ensemble import Ensemble
+
+
+class NESBayesianSampler:
+    def __init__(self):
+        pass
+
+    def sample_mc(self, val_loader: DataLoader) -> Ensemble:
+        """
+        Args:
+            val_loader (DataLoader): Used to evaluate the posterior.
+
+        Returns:
+            Ensemble: The final ensemble wrapped in bensemble's core abstraction.
+        """
+        pass
+        # models = [model1, model2, ...]
+        # return Ensemble.from_models(models)
+
+    def sample_svgd(
+        self,
+        val_loader: DataLoader,
+    ) -> Ensemble:
+        """
+        Args:
+            val_loader (DataLoader): Used to evaluate the architecture's loss/posterior.
+
+        Returns:
+            Ensemble: The final ensemble wrapped in bensemble's core abstraction.
+        """
+        pass
+        # models = [model1, model2, ...]
+        # return Ensemble.from_models(models)
