9bc208a5b637edb398db6a177cbeaf7558c047fb
Muhammadsharif Nabiev <mikhmed.work@gmail.com>
2026-07-17 17:28:15 +0300

    bug: fixed imports for `decomposition` module

---
+++ bensemble/uncertainty/__init__.py
@@ -0,0 +1,6 @@
+from .decomposition import (
+    decompose_classification_uncertainty,
+    decompose_regression_uncertainty,
+)
+
+__all__ = ["decompose_classification_uncertainty", "decompose_regression_uncertainty"]
